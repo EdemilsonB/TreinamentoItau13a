32f97a741cbdee36a63f9f0d741b264e8c1be91e
EdemilsonB <edemilson.b@hotmail.com>
2021-01-14 15:01:05 -0300

    teste 3 php

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
-<p>According to the <a href="/en-US/docs/Web/HTML/Element/blockquote">
-<cite>MDN blockquote page</cite></a>:
-</p>
+<H1> Hello World </H1>
+<H6> Olá Mundo </H6>
 
-<blockquote cite="/en-US/docs/Web/HTML/Element/blockquote">
-  <p>The <strong>HTML <code>&lt;blockquote&gt;</code> Element</strong> (or <em>HTML Block
-  Quotation Element</em>) indicates that the enclosed text is an extended quotation.</p>
-</blockquote>
-
-<p>The quote element — <code>&lt;q&gt;</code> — is <q cite="/en-US/docs/Web/HTML/Element/q">intended
-for short quotations that don't require paragraph breaks.</q> -- <a href="/en-US/docs/Web/HTML/Element/q">
-<cite>MDN q page</cite></a>.</p>
+<a href="https://www.w3schools.com/">Clique aqui para me chamar no WhatsApp!</a>
