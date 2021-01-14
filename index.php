fb01d7f8407bb560e5e2bff6e76021ba440dae24
Edemilson Bolanho <31864224+EdemilsonB@users.noreply.github.com>
2021-01-14 14:41:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-Aula ensinando a fazer um deploy simples com CD
+<p>According to the <a href="/en-US/docs/Web/HTML/Element/blockquote">
+<cite>MDN blockquote page</cite></a>:
+</p>
+
+<blockquote cite="/en-US/docs/Web/HTML/Element/blockquote">
+  <p>The <strong>HTML <code>&lt;blockquote&gt;</code> Element</strong> (or <em>HTML Block
+  Quotation Element</em>) indicates that the enclosed text is an extended quotation.</p>
+</blockquote>
+
+<p>The quote element — <code>&lt;q&gt;</code> — is <q cite="/en-US/docs/Web/HTML/Element/q">intended
+for short quotations that don't require paragraph breaks.</q> -- <a href="/en-US/docs/Web/HTML/Element/q">
+<cite>MDN q page</cite></a>.</p>
